264a81e521db1c3c5c2b9d6bf9e9bfe3244ff458
Lucas Beserra <lbeserra.under@gmail.com>
2025-06-09 22:18:58 -0300

    Atualizando a migration de clients e products

--- src/migrations/20250609024716-create-clients.js
+++ src/migrations/20250609024716-create-clients.js
@@ -3,20 +3,30 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('clients', {
+      client_id: {
+        type: Sequelize.UUIDV4,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('clients');
   }
 };
